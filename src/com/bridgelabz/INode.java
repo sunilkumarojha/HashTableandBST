e9a1111c9a1bef86112372cc4b1d914ae4a654ee
sunilkumarojha <100593013+sunilkumarojha@users.noreply.github.com>
2022-03-30 21:36:33 +0530

    Adding Element

---
+++ src/com/bridgelabz/INode.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class INode {
+    public class INode {
+        int key;
+        INode left, right;
+
+        public INode(int data) {
+            key = data;
+            left = right = null;
+        }
+    }
+}
---
+++ src/com/bridgelabz/main.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println();
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(56);
+        bst.insert(30);
+        bst.insert(70);
+
+        bst.display();
+    }
+}
